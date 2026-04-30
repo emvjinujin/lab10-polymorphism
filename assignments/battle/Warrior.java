c8ca6728c2a2b1eccb8ba3c55e3df5bfeec891fb
b24fm1032 <emvjn44@gmail.com>
2026-04-30 08:16:32 -0500

    Update Warrior.java

--- assignments/battle/Warrior.java
+++ assignments/battle/Warrior.java
@@ -1,12 +1,22 @@
 public class Warrior extends Character {
 
-    // TODO: Constructor бичнэ үү
-    // public Warrior(String name, int hp)
-    // - super(name, hp) дуудна
+    public Warrior(String name, int hp) {
+        super(name, hp);
+    }
 
-    // TODO: @Override attack(Character target) → int
-    // - 20 хохирол учруулна (target.takeDamage(20))
-    // - 20-г буцаана
-    // - Энэ нь parent-ын default 10 damage-ийг override хийнэ
+    @Override
+    public int attack(Character target) {
+        int damage = 20;
+        target.takeDamage(damage);
+        
+        System.out.println(this.getName() + " swings a mighty sword at " + target.getName() + "!");
+        
+        return damage;
+    }
 
+   
+    @Override
+    public String describe() {
+        return "wields a sword";
+    }
 }
